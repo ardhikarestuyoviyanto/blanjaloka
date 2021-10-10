89488a9396a4cf3233fdc605ad56c5f8ef24b36d
ardhikarestu yoviyanto <ardhikayoviyanto@gmail.com>
2021-10-10 21:33:45 +0700

    Ubah salah satu kolom di tabel users, nullable

--- database/migrations/2021_10_09_041228_create_users_table.php
+++ database/migrations/2021_10_09_041228_create_users_table.php
@@ -16,10 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id('id_users');
             $table->string('nama_user', 100);
-            $table->string('no_telp', 20);
+            $table->string('no_telp', 20)->nullable();
             $table->string('email', 100);
             $table->string('password', 200);
-            $table->text('alamat');
+            $table->text('alamat')->nullable();
             $table->enum('status', ['on', 'off']);
             $table->timestamps();
         });
