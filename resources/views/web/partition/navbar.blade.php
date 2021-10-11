7149d571feb4ddea10763bddc56500072b1b8c02
ardhikarestu yoviyanto <ardhikayoviyanto@gmail.com>
2021-10-11 18:35:39 +0700

    Set navbar users -> if users login atau tidak -> navbar akan berbeda

--- resources/views/web/partition/navbar.blade.php
+++ resources/views/web/partition/navbar.blade.php
@@ -19,6 +19,7 @@
             </div>
         </form>
         
+        @if(!session('isUsers'))
         <ul class="navbar-nav ms-auto">
             <li class="nav-item">
             <a class="btn cai-color-text fs-3 m-1 ms-3" id="keranjang"><i class="bi bi-cart3"></i></a>
@@ -27,6 +28,32 @@
             <a class="btn cai-color text-white m-1" id="buttonDaftar" href="{{url('register')}}">Daftar</a>
             </li>
         </ul>
+        @else
+
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+            <a class="btn cai-color-text fs-3 m-1 ms-3" id="keranjang"><i class="bi bi-cart3"></i></a>
+            <a class="garisBatas fs-3 text-secondary align-middle me-3">|</a>
+            <a class="btn bi bi-bell-fill cai-color-text m-1 fs-5" id="buttonMasuk" aria-current="page" href="notifikasi.html"><span class="ms-2 fs-6 align-middle">Notifikasi</span></a>
+            
+            <div class="dropdown d-inline">
+                <button class="btn bi bi-person-circle cai-color-text fs-4" type="button" id="dropdownUser" data-bs-toggle="dropdown" aria-expanded="false">
+                  <p class="fs-6 align-middle d-inline">{{session()->get('nama_user')}}</p>
+                </button>
+                <ul class="dropdown-menu p-3 mt-0 mt-xl-4" aria-labelledby="dropdownUser">
+                    <li><span class="dropdown-item-text fw-bold">Halo, {{session()->get('nama_user')}}</span></li>
+                  <li><a class="dropdown-item" href="#">Pesan</a></li>
+                  <li><a class="dropdown-item" href="#">Favorit</a></li>
+                  <li><a class="dropdown-item" href="#">Daftar Belanja</a></li>
+                  <li><a class="dropdown-item" href="#">Pesanan Saya</a></li>
+                  <li><a class="dropdown-item" href="#">Pengaturan</a></li>
+                  <li><a class="dropdown-item" href="{{url('logout')}}">Keluar</a></li>
+                </ul>
+              </div>
+            </li>
+        </ul>
+        @endif
+
         </div>
     </div>
 </nav>
