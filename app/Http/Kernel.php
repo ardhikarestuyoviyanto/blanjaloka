19ac709fc342ff9a6f5179f2d95dbde5ef4bb8d5
ardhikarestu yoviyanto <ardhikayoviyanto@gmail.com>
2021-10-11 18:26:48 +0700

    Set route Middleware, sessionusers

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        # Tambah class middleware sessionusers
+        'sessionusers' => \App\Http\Middleware\SessionUsers::class
     ];
 }
