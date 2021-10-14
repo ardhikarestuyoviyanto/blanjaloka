22bf75bf34a3819fc3337a618adda6124fc2526a
ardhikarestu yoviyanto <ardhikayoviyanto@gmail.com>
2021-10-14 20:09:34 +0700

    tambah kolom logo_toko dan nama_toko di tabel penjual

---
+++ database/migrations/2021_10_14_092512_add_namatoko_logotoko_to_penjual.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNamatokoLogotokoToPenjual extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('penjual', function (Blueprint $table) {
+            //
+            $table->string('logo_toko', 200)->nullable();
+            $table->string('nama_toko', 200)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('penjual', function (Blueprint $table) {
+            //
+            $table->dropColumn(['logo_toko', 'nama_toko']);
+        });
+    }
+}
