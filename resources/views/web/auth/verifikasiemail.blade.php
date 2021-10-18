598b1c5198271522d9f5f7c93b33f11b978d9ced
ardhikarestu yoviyanto <ardhikayoviyanto@gmail.com>
2021-10-18 20:08:24 +0700

    styling templeate verifikasi email

--- resources/views/web/auth/verifikasiemail.blade.php
+++ resources/views/web/auth/verifikasiemail.blade.php
@@ -6,6 +6,21 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Email From Blanjaloka</title>
     <style>
+        .card {
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+        transition: 0.3s;
+
+        }
+        .judul{
+            text-align: center;
+        }
+
+        .card:hover {
+        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+        }
+        .container {
+        padding: 2px 16px;
+        }
         a:link, a:visited {
             background-color: rgb(60, 72, 179);
             color: white;
@@ -14,15 +29,19 @@
             text-decoration: none;
             display: inline-block;
         }
-
         a:hover, a:active {
             background-color: rgb(60, 72, 179);
         }
     </style>
 <body>
-    <h1>Aktifasi Akun Blanjaloka</h1>
-    <p>Hai, {{$details['nama_user']}}. Terimakasih telah memilih Blanjaloka Sebagai Platform Jual Beli anda, Sebelum Lanjut Belanja Verifikasi Emailmu dulu yuk</p>
-    <p><a type="button" href="{{url('verification/'.$details['email'].'/'.$details['token'])}}">Aktifasi Email</a></p>
-    <p>Thank You</p>
+
+    <div class="card">
+        <div class="container">
+            <h1>Aktifasi Akun Blanjaloka</h1>
+            <p>Hai, {{$details['nama_user']}}. Terimakasih telah memilih Blanjaloka Sebagai Platform Jual Beli anda, Sebelum Lanjut Belanja Verifikasi Emailmu dulu yuk</p>
+            <p><a type="button" href="{{url('verification/'.$details['email'].'/'.$details['token'])}}">Aktifasi Email</a></p>
+            <p>Thank You</p>
+        </div>
+    </div>
 </body>
 </html>
